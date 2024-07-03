f4318365c254db7be6dc9f45e501d00e72b482b1
Ramirez Diaz Carlos <cer_79@hotmail.com>
2024-07-03 00:20:14 -0300

    funcion para llamar API

--- js/common.js/common.js
+++ js/common.js/common.js
@@ -1,43 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const apiBaseURL = 'https://dolarapi.com/v1/cotizaciones';
+    const apiDollarURL = 'https://dolarapi.com/v1/dolares'; 
 
+    function fetchCotizaciones(moneda) {
+        let url = '';
+        switch (moneda) {
+            case 'dolar':
+                url = apiDollarURL;
+                break;
+            case 'eur':
+                url = `${apiBaseURL}/eur`;
+                break;
+            case 'brl':
+                url = `${apiBaseURL}/brl`;
+                break;
+            case 'peso_chileno':
+                url = `${apiBaseURL}/clp`;
+                break;
+            case 'peso_uruguayo':
+                url = `${apiBaseURL}/uyu`;
+                break;
+            case 'TODAS':
+                url = apiBaseURL;
+                break;
+            default:
+                return;
+        }
 
-fetch("https://dolarapi.com/v1/dolares/oficial")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_oficial").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_oficial").textContent = `$${data.venta}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                actualizarCotizaciones(moneda, data);
+            })
+    }
+    
+    function actualizarCotizaciones(moneda, data) {
+        const contenedor = document.getElementById('exchangeRates');
+        contenedor.innerHTML = ''; // Limpiar contenido existente
+    
+        const crearDivCotizacion = (titulo, compra, venta) => {
+            const div = document.createElement('div');
+            div.classList.add('contenedor_precio');
+    
+            div.innerHTML = `
+                <div class="lado-izquierdo"><p>${titulo}</p></div>
+                <div class="lado-derecho">
+                    <div><p>Compra</p><p class="precio">${compra}</p></div>
+                    <div><p>Venta</p><p class="precio">${venta}</p></div>
+                </div>
+            `;
+            contenedor.appendChild(div);
+        };
+
+        if (data) {
+            if (Array.isArray(data)) {
+                data.forEach(item => {
+                    crearDivCotizacion(item.nombre, item.compra, item.venta);
+                });
+            } else {
+                crearDivCotizacion(data.nombre, data.compra, data.venta);
+            }
+        }
+    }
+
+    const monedasSelect = document.getElementById('monedas');
+    monedasSelect.addEventListener('change', (event) => {
+        fetchCotizaciones(event.target.value);
     });
 
-fetch("https://dolarapi.com/v1/dolares/blue")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_blue").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_blue").textContent = `$${data.venta}`;
-    });
-
-fetch("https://dolarapi.com/v1/dolares/tarjeta")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_tarjeta").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_tarjeta").textContent = `$${data.venta}`;
-    });
-
-fetch("https://dolarapi.com/v1/dolares/bolsa")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_mep").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_mep").textContent = `$${data.venta}`;
-    });
-
-fetch("https://dolarapi.com/v1/dolares/contadoconliqui")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_ccl").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_ccl").textContent = `$${data.venta}`;
-    });
-
-fetch("https://dolarapi.com/v1/dolares/cripto")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("precio_compra_cripto").textContent = `$${data.compra}`;
-        document.getElementById("precio_venta_cripto").textContent = `$${data.venta}`;
-    });
+    fetchCotizaciones(monedasSelect.value);
+});
